dc6ee18202bc4071aab8d9c20a23195f6fee6b97
Esben Kran <esben@kran.ai>
2022-08-31 23:39:40 +0200

    edit link fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,7 +32,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/apartresearch/docs",
+          editUrl: "https://github.com/apartresearch/docs/tree/main/",
         },
         blog: false,
         theme: {
